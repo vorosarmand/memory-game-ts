bcb84f9dbc4fcff1754a088812f15b6cbdef1b8c
ARMAND VÖRÖS <armandvoros20@gmail.com>
2025-07-15 17:05:56 +0200

    feat: Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FC } from "react";
+
+interface ButtonProps {
+  onClick: () => void;
+  text?: string;
+  icon?: IconDefinition;
+}
+
+const Button: FC<ButtonProps> = ({ onClick, text, icon }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="cursor-pointer text-[#D5D5D5] hover:text-gray-200 w-8 h-8"
+    >
+      {icon ? <FontAwesomeIcon icon={icon} size="xl" /> : null}
+      {text ? text : null}
+    </button>
+  );
+};
+
+export default Button;
